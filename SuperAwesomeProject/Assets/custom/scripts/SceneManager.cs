3b68f4aaa8910604be6c7eb46a91d4914df2d306
unknown <ajh2510@C2435-22.main.ad.rit.edu>
2014-11-15 00:49:34 -0500

    Added car and driving along

---
+++ SuperAwesomeProject/Assets/custom/scripts/SceneManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	GameObject terrainChunk1;
+	GameObject terrainChunk2;
+	public GameObject terrainChunk;
+	public float carSpeed= 100.0f;
+	// Use this for initialization
+	void Start () {
+		terrainChunk1= (GameObject)GameObject.Instantiate(terrainChunk,new Vector3(0,0,0),new Quaternion());
+		terrainChunk2 = (GameObject)GameObject.Instantiate(terrainChunk,new Vector3(1000,0,0),new Quaternion());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 curLocation = terrainChunk1.transform.position;
+		curLocation.x+= carSpeed*Time.deltaTime;
+		if(curLocation.x>1000.0){
+			float diff=curLocation.x-1000.0f;
+			curLocation.x=-1000.0f+diff;
+		}
+		terrainChunk1.transform.position=curLocation;
+		curLocation = terrainChunk2.transform.position;
+		curLocation.x+= carSpeed*Time.deltaTime;
+		if(curLocation.x>1000.0){
+			float diff=curLocation.x-1000.0f;
+			curLocation.x=-1000.0f+diff;
+		}
+		terrainChunk2.transform.position=curLocation;
+	}
+}
