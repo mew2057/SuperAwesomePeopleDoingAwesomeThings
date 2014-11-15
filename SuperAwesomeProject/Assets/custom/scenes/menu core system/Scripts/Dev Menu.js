dc08bd94c77a7ee8bfdc3611c467f43fafdc52ce
Nick Buonarota <NickyDaB@gmail.com>
2014-11-14 21:42:44 -0500

    nick added menu system start

---
+++ SuperAwesomeProject/Assets/custom/scenes/menu core system/Scripts/Dev Menu.js
@@ -0,0 +1,40 @@
+﻿var isMainMenu=false;
+var isPrevious=false;
+var isNext=false;
+
+function OnMouseEnter(){
+	//change text color
+	renderer.material.color=Color.red;
+}
+
+function OnMouseExit(){
+	//change text color
+	renderer.material.color=Color.white;
+}
+
+function OnMouseUp(){
+	//is this quit
+	if (isMainMenu==true) {
+		//quit the game
+		Application.LoadLevel(0);
+	}
+	if (isPrevious==true) {
+		//quit the game
+		//Application.LoadLevel(X);
+	}
+	if (isNext==true) {
+		//quit the game
+		//Application.LoadLevel(X);
+	}
+	else {
+		//load level
+		//Application.LoadLevel(X);
+	}
+}
+
+function Update(){
+	//quit game if escape key is pressed
+	if (Input.GetKey(KeyCode.Escape)) {
+    		Application.Quit();
+	}
+}
