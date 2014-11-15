6692993b8db29802d7c56196fd8e08f6e3ce377d
John Dunham <JFDBear>
2014-11-15 14:59:31 -0500

    Billboards with OVR

--- SuperAwesomeProject/Assets/custom/scripts/Billboard.js
+++ SuperAwesomeProject/Assets/custom/scripts/Billboard.js
@@ -1,16 +1,15 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿#pragma strict
 
-public class Billboard : MonoBehaviour {
+public class Billboard extends MonoBehaviour {
 
-	public GameObject cameraToSeek;
+	public var cameraToSeek:GameObject;
 	// Use this for initialization
-	void Start () {
+	function Start () {
 		cameraToSeek = GameManager.Instance.MainCamera;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	function Update () {
 		transform.LookAt(new Vector3(cameraToSeek.transform.position.x,this.transform.position.y,cameraToSeek.transform.position.z));
 		transform.Rotate(new Vector3(0.0f,180.0f,0.0f));
 	}
