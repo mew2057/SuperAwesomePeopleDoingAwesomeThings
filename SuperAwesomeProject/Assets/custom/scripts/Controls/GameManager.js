e2d41323916d21218dfc9b392949eca21511a1e9
John Dunham <JFDBEar1@gmail.com>
2014-11-15 08:09:18 -0500

    Moved stuff to a GameManager singleton for some work I'll be doing in a bit.

---
+++ SuperAwesomeProject/Assets/custom/scripts/Controls/GameManager.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+public enum GameState
+{
+	Waiting,
+	Driving,
+	Question, // A Question is on screen.
+	Answered, 
+	Failed
+}
+
+public class GameManager extends MonoBehaviour
+{	
+	// The Game State
+	public var state:GameState = GameState.Waiting;
+	public var player:Player;
+	
+	public static var Instance:GameManager;
+
+	
+	function Awake()
+	{
+		Instance = this;
+		this.state = GameState.Waiting;
+		
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Reacts to context switching.
+	function AnswerRecieved()
+	{
+		if(this.state != GameState.Failed)
+		{
+			player.AnswerRecieved();
+		}
+
+	}
+	
+	function Start () {
+		
+	}
+
+	function Update () {
+
+	}
+}
